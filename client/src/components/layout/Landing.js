e87ace6d02619797ca3176a9e5be3f85cd12f26c
mark-escosura <escosuramarkse@gmail.com>
2022-02-06 20:40:57 -0800

    Implemented NavBar and Landing page

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Connect Within</h1>
+          <p className='lead'>
+            Connect with developers from all around the world.
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
